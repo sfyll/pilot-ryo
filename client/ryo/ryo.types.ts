6c7d783a3b0bfdd12b6ad5bbe4d92870a620f5bb
SFYLL <santiagoflood@hotmail.fr>
2024-03-10 18:29:42 +0100

    ryo types

--- client/ryo/ryo.types.ts
+++ client/ryo/ryo.types.ts
@@ -2,18 +2,54 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import {
-    createStarknetEIP712DomainType,
-    createEIP712TypesNoBody,
+    createStarknetEIP712DomainType, 
+    createEIP712TypesNoBodyStarknet,
 } from "../interfaces/eip712.interface";
+import { StarkNetDomain, TypedData } from "starknet";
+
+/*
+ * helper function to construct TypedData as specified in EIP712.
+ */
+export function getStarknetTypedData(
+    types: any,
+    primaryType: string, 
+    domain:  StarkNetDomain,
+): {types: any, primaryType: string, domain: StarkNetDomain }   {
+    return {
+        types, 
+        primaryType, 
+        domain,
+    }
+}
+
+/*
+ * helper function to construct TypedData as specified in EIP712, including the message.
+ */
+export function getStarknetTypedDataWithMessage(
+    types: any,
+    primaryType: string, 
+    domain:  StarkNetDomain,
+    message: any,
+): TypedData {
+    const typedDataWithoutMessage = getStarknetTypedData(types, primaryType, domain)
+    return {
+        ...typedDataWithoutMessage,
+        message
+    }
+}
+export const nonce = { name: "nonce", type: "felt"};
+
 
 export const TradeParametersTypes = [
+    nonce,
     { name: "player_id", type: "felt" },
     { name: "game_id", type: "felt" }
 ];
 
 export const tradeParametersActionTypeLabel = "TradeParameters";
-export let tradeParametersActionTypes = createEIP712TypesNoBody(tradeParametersActionTypeLabel, TradeParametersTypes);
-export const tradeParametersActionDomain = createStarknetEIP712DomainType("Trade Parameters Action");
+export const tradeParametersActionTypes = createEIP712TypesNoBodyStarknet(tradeParametersActionTypeLabel, TradeParametersTypes);
+export const tradeParametersActionDomain = createStarknetEIP712DomainType("Trade Parameters Request");
 
+export const tradeParametersDAReqTyped = getStarknetTypedData(tradeParametersActionTypes, tradeParametersActionTypeLabel, tradeParametersActionDomain)
 
 
