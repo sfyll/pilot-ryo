1786fe7f442fde6626835aa300433fc455f427bf
SFYLL <santiagoflood@hotmail.fr>
2024-03-10 18:28:46 +0100

    starknet auth middleware

--- client/middleware/auth.middleware.ts
+++ client/middleware/auth.middleware.ts
@@ -1,11 +1,13 @@
 import { NextFunction, Request, Response, RequestHandler } from "express";
-
-import { EIP712Types, EIP712DomainType, StarknetEIP712DomainType } from "../interfaces/eip712.interface";
+import { EIP712DomainType, EIP712Types } from "../interfaces/eip712.interface";
 import { recoverTypedMessageAddress } from "../utils/signature";
 import { handleAsync } from "../utils/error";
 import AuthenticationService from "../authentication/authentication.service";
-import { RequestWithSignature } from "../interfaces/request.interface";
+import { RequestWithSignature, StarknetRequestWithSignature } from "../interfaces/request.interface";
 import InvalidSignatureException from "../exceptions/InvalidSignatureException";
+import { StarkNetDomain, typedData } from "starknet";
+import { getStarknetTypedDataWithMessage } from "../ryo/ryo.types";
+import { getContractAccount } from "../ryo/utils/starknet_handler";
 
 /*
  * Authenticates an incoming request by checking whether the singer has not
@@ -45,20 +47,39 @@ export function authMiddleware<T>(
     return auth;
 }
 
-export function starknetAuthhMiddleware<T>(
+/*
+ * Authenticates an incoming request by checking whether the singer has not
+ * used this nonce before, applied to starknet signatures.
+ */
+export  function starknetAuthhMiddleware<T>(
     dataTypes: EIP712Types,
     dataPrimary: string,
-    dataDomain: StarknetEIP712DomainType,
+    dataDomain: StarkNetDomain,
 ): RequestHandler {
     async function auth(
-        request: RequestWithSignature,
+        request: StarknetRequestWithSignature,
         response: Response,
         next: NextFunction,
     ) {
-        console.log(dataTypes);
-        console.log(dataPrimary);
-        console.log(dataDomain);
-           }
+        const contractAccount = getContractAccount(request.body.tx.player_id);
+
+        let typedDataValidate = getStarknetTypedDataWithMessage(dataTypes, dataPrimary, dataDomain, request.body.tx);
+
+        const msgHash = typedData.getMessageHash(typedDataValidate, request.body.tx.player_id); 
+        
+        try {
+          await contractAccount.isValidSignature(msgHash, [request.body.signature.r, request.body.signature.s]);
+          const authenticationService = request.app.get(
+            "authenticationService",
+        ) as AuthenticationService;
+
+        request.body["sender"] = request.body.tx.player_id;
+        next();
+        } catch (error) {
+            next(new InvalidSignatureException());
+            return;
+        }
+    }
     return auth;
 }
 
